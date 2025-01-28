a12037e9896a574527d644a11522db166074efb6
Tornyai Laurentiu Andrei <121413180+thedrew94@users.noreply.github.com>
2025-01-28 11:12:38 +0100

    Update test5.js

--- test5.js
+++ test5.js
@@ -1,71 +1,106 @@
-// todoList.test.js
-const todoList = require("./todoList");
+// invoice.js
+class Invoice {
+    constructor(id, date, amount, description) {
+      this.id = id;
+      this.date = date;
+      this.amount = amount;
+      this.description = description;
+    }
+  }
+  
+  module.exports = Invoice;
 
-describe("todolist ", () => {
-  test("should add a task to the list", () => {
-    const list = todoList();
-    list.addTask("Buy milk");
-    expect(list.listTasks()).toEqual(["Buy milk"]);
-  });
+// invoiceManager.js
+const Invoice = require("./invoice");
 
-  test("should remove a task from the list", () => {
-    const list = todoList();
-    list.addTask("Buy milk");
-    list.addTask("Walk dog");
-    list.removeTask("Buy milk");
-    expect(list.listTasks()).toEqual(["Walk dog"]);
-  });
-
-  test("mark a task as completed", () => {
-    const list = todoList();
-    list.addTask("Buy milk");
-    list.completeTask("Buy milk");
-    expect(list.listTasks()).toEqual([{ task: "Buy milk", completed: true }]);
-  });
-
-  test("should list tasks including their completion status", () => {
-    const list = todoList();
-    list.addTask("Buy milk");
-    list.addTask("Walk dog");
-    list.completeTask("Buy milk");
-    expect(list.listTasks()).toEqual([
-      { task: "Buy milk", completed: true },
-      { task: "Walk dog", completed: false },
-    ]);
-  });
-});
-
-// todoList.js
-function todoList() {
-  let tasks = [];
-
-  function addTask(task) {
-    tasks.push({ task: task, completed: false });
+class InvoiceManager {
+  constructor() {
+    this.invoices = [];
   }
 
-  function removeTask(task) {
-    tasks = tasks.filter((t) => t.task !== task);
+  // Create
+  addInvoice(date, amount, description) {
+    const id = this.invoices.length + 1;
+    const invoice = new Invoice(id, date, amount, description);
+    this.invoices.push(invoice);
+    return invoice;
   }
 
-  function completeTask(task) {
-    tasks = tasks.map((t) => {
-      if (t.task === task) {
-        return { ...t, completed: true };
-      }
-      return t;
-    });
+  // Read
+  getInvoice(id) {
+    return this.invoices.find((invoice) => invoice.id === id);
   }
 
-  function listTasks() {
-    return tasks;
+  // Update
+  updateInvoice(id, date, amount, description) {
+    const index = this.invoices.findIndex((invoice) => invoice.id === id);
+    if (index !== -1) {
+      this.invoices[index] = new Invoice(id, date, amount, description);
+      return this.invoices[index];
+    }
+    return null;
   }
 
-  return {
-    addTask,
-    removeTask,
-    completeTask,
-    listTasks,
-  };
+  // Delete
+  deleteInvoice(id) {
+    const index = this.invoices.findIndex((invoice) => invoice.id === id);
+    if (index !== -1) {
+      return this.invoices.splice(index, 1)[0];
+    }
+    return null;
+  }
+
+  // Get all invoices
+  getAllInvoices() {
+    return this.invoices;
+  }
 }
 
-module.exports = todoList;
+module.exports = InvoiceManager;
+
+// invoiceManager.test.js
+const InvoiceManager = require("./invoiceManager");
+
+describe("InvoiceManager", () => {
+  let manager;
+
+  beforeEach(() => {
+    manager = new InvoiceManager();
+  });
+
+  test("addInvoice should add a new invoice", () => {
+    const invoice = manager.addInvoice("2025-01-01", 100, "Test Invoice");
+    expect(invoice.id).toBe(1);
+    expect(invoice.date).toBe("2025-01-01");
+    expect(invoice.amount).toBe(100);
+    expect(invoice.description).toBe("Test Invoice");
+  });
+
+  test("getInvoice should return the correct invoice", () => {
+    manager.addInvoice("2025-01-01", 100, "Test Invoice");
+    const invoice = manager.getInvoice(1);
+    expect(invoice.id).toBe(1);
+  });
+
+  test("updateInvoice should update an existing invoice", () => {
+    manager.addInvoice("2025-01-01", 100, "Test Invoice");
+    const updatedInvoice = manager.updateInvoice(1, "2025-01-02", 200, "Updated Invoice");
+    expect(updatedInvoice.id).toBe(1);
+    expect(updatedInvoice.date).toBe("2025-01-02");
+    expect(updatedInvoice.amount).toBe(200);
+    expect(updatedInvoice.description).toBe("Updated Invoice");
+  });
+
+  test("deleteInvoice should delete the correct invoice", () => {
+    manager.addInvoice("2025-01-01", 100, "Test Invoice");
+    const deletedInvoice = manager.deleteInvoice(1);
+    expect(deletedInvoice.id).toBe(1);
+    expect(manager.getAllInvoices().length).toBe(0);
+  });
+
+  test("getAllInvoices should return all invoices", () => {
+    manager.addInvoice("2025-01-01", 100, "Test Invoice");
+    manager.addInvoice("2025-01-02", 200, "Another Invoice");
+    expect(manager.getAllInvoices().length).toBe(2);
+  });
+});
